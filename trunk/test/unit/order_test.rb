c530f498a606cd807a37dbcc9589377fdefb0d45
kamil.skalski <kamil.skalski@4afc3f5b-422b-0410-834f-d720f26963ec>
2007-03-11 21:00:16 +0000

    Statistics, fixes

--- trunk/test/unit/order_test.rb
+++ trunk/test/unit/order_test.rb
@@ -1,10 +1,36 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class OrderTest < Test::Unit::TestCase
-  fixtures :orders
+  fixtures :suppliers, :materials, :orders, :order_positions
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_release
+    order = Order.find(1)
+    num_deliveries = FoodMailer.deliveries.size
+    order.release
+    assert_equal num_deliveries + 1, ActionMailer::Base.deliveries.size
+  end
+  
+  def test_sumworth
+    order = Order.find(orders(:sumworth).id)
+    assert_equal 4, order.order_positions.length
+    assert_in_delta 11.41, order.total_value, 0.0001
+  end
+  
+  def test_material_stat
+    order = Order.find(orders(:sumworth).id)
+    stat = order.material_stats
+    assert_equal 'Foo1', stat[0][0]
+    assert_equal 1, stat[0][1]
+    assert_equal 'Foo2', stat[1][0]
+    assert_equal 2, stat[1][1]
+    assert_equal 'Foo3', stat[2][0]
+    assert_equal 1, stat[2][1]
+  end
+  
+  def test_received
+    order = Order.find(orders(:sumworth).id)
+    num_deliveries = FoodMailer.deliveries.size
+    order.received
+    assert_equal num_deliveries + 4, ActionMailer::Base.deliveries.size
   end
 end
