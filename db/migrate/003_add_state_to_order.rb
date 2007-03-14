3a661f84269e2c731e0a68497d537a3b76fc5427
kamil.skalski <kamil.skalski@4afc3f5b-422b-0410-834f-d720f26963ec>
2007-03-14 22:10:06 +0000

    Add missing migration script

---
+++ db/migrate/003_add_state_to_order.rb
@@ -0,0 +1,9 @@
+class AddStateToOrder < ActiveRecord::Migration
+  def self.up
+    add_column :orders, :state, :integer, :default => 4, :null => false
+  end
+
+  def self.down
+    remove_column :orders, :state
+  end
+end
